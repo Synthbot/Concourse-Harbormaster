d60e608dbda6eccd29cdf2f13086d0828d81f4b0
Synthbot <synthbot@tutanota.com>
2019-08-18 03:16:40 -0400

    added missing ./ for changing directories

--- scripts/barge.sh
+++ scripts/barge.sh
@@ -9,19 +9,19 @@ try() { "$@" || die "cannot $*"; }
 
 SEPARATE_DOCKER_COMPOSE="false"
 
-ls -lRah --color
+# ls -lRah --color
 
 [ -d ./docker-compose-source ] && echo "Directory for docker-compose-source exists" && SEPARATE_DOCKER_COMPOSE="true"
 [ -d ./github-source ] && echo "Directory for github-source exists" || echo "No github-source found!" && exit 101
 
+echo $SEPARATE_DOCKER_COMPOSE
+
 if [ "$SEPARATE_DOCKER_COMPOSE" = "true" ];
 then
-   cd docker-compose-source 
+   cd ./docker-compose-source 
 else
-   cd github-source 
+   cd ./github-source 
 fi
-
-echo $SEPARATE_DOCKER_COMPOSE
 
 find ./path -iname 'dockerfile' -type f || echo "No dockerfile found!" && exit 101
 
