e2743addda840a91e605ca373b8c61de4c3bea56
Synthbot <synthbot@tutanota.com>
2019-08-18 03:07:36 -0400

    Updated script again

--- scripts/barge.sh
+++ scripts/barge.sh
@@ -3,12 +3,16 @@ set -e
 DIR=$( dirname "${BASH_SOURCE[0]}" )
 echo "$DIR"
 
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "cannot $*"; }
+
 SEPARATE_DOCKER_COMPOSE="false"
 
 ls -lRah --color
 
-[ -d /docker-compose-source ] && echo "Directory for docker-compose-source exists" && SEPARATE_DOCKER_COMPOSE="true"
-[ -d /github-source ] && echo "Directory for github-source exists" || echo "No github-source found!" && exit
+[ -d ./docker-compose-source ] && echo "Directory for docker-compose-source exists" && SEPARATE_DOCKER_COMPOSE="true"
+[ -d ./github-source ] && echo "Directory for github-source exists" || echo "No github-source found!" && exit 101
 
 if [ "$SEPARATE_DOCKER_COMPOSE" = "true" ];
 then
@@ -19,7 +23,7 @@ fi
 
 echo $SEPARATE_DOCKER_COMPOSE
 
-find /path -iname 'dockerfile' -type f || echo "No dockerfile found!" && exit
+find ./path -iname 'dockerfile' -type f || echo "No dockerfile found!" && exit 101
 
 docker build -t $DOCKER_IMAGE_NAME .
 docker image ls
